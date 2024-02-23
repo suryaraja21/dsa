80f7fc078fd3e41bb0025c89f6b25b3e2f8af243
suryaraja21 <160419610+suryaraja21@users.noreply.github.com>
2024-02-23 12:54:26 +0530

    Add files via upload

---
+++ evaluation of postfix.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 20
+void eval();
+void push(int);
+int pop();
+char postfix[20];
+struct stack
+{
+	int s[size];
+	int top;
+}st;
+main(){
+	st.top=-1;
+
+	printf("enter the postfix expression :");
+	scanf("%s",&postfix);
+	eval();
+	printf("%d",st.s[st.top]);
+}
+
+
+void eval(){
+	int i,op1,op2;
+	for(i=0;postfix[i]!='\0';i++)
+	{
+	switch(postfix[i])
+	{
+	case '+':
+	{
+	char res;
+	op1=pop();
+	op2=pop();
+	res=(op2)+(op1);
+	push(res);
+	break;
+	}
+	case '-':
+	{
+	char res;
+	op1=pop();
+	op2=pop();
+	res=(op2)-(op1);
+	push(res);
+	break;
+	}
+	case '*':
+	{
+	char res;
+	op1=pop();
+	op2=pop();
+	res=(op2)*(op1);
+	push(res);
+	break;
+	}
+	default 
+	:push(postfix[i]-48);
+	}
+	}
+	}
+	void push(int ele)
+	{
+	st.top++;
+	st.s[st.top]=ele;
+	}
+	int pop()
+	{
+	char el;
+	el=st.s[st.top];
+	st.top--;
+	return el;
+	}
